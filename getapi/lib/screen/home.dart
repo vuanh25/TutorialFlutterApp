97ca992b0a4eb64f7769d4b9c376528174454ec9
VuCoder <chimcudammua@gmail.com>
2023-04-03 11:53:21 +0700

    getapi

---
+++ getapi/lib/screen/home.dart
@@ -0,0 +1,56 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<dynamic> users = [];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:  AppBar(
+        title: const Text('Rest API Call'),
+      ),
+      body: ListView.builder(
+        itemCount:  users.length,
+        itemBuilder: (context, index) {
+          final user = users[index];  
+          final name = user['name']['first'];
+          final email = user['email'];
+          final imageUrl = user['picture']['thumbnail'];
+          return ListTile(
+            leading:ClipRRect(
+              borderRadius: BorderRadius.circular(100),
+              child:Image.network(imageUrl)), 
+            title: Text(name),
+            subtitle: Text(email),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: fetchUsers,
+      ),
+    );
+  }
+
+  void fetchUsers() async{
+    print('fetching users...'); 
+    const url = 'https://randomuser.me/api/?results=100';
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    final body = response.body;
+    final json = jsonDecode(body);
+    setState(() {
+       users = json['results'];
+    });
+    print('getachUsers complete...'); 
+  }
+}
